7133704d28e5766ee63ae4c9900c3d36cfca80b5
matheus.chang <matheus.chang@ip-192-168-0-80.ec2.internal>
2023-03-22 11:11:17 -0300

    new routes

--- app.py
+++ app.py
@@ -1,8 +1,16 @@
 from flask import Flask, request
 app = Flask(__name__)
 
+
+@app.route('/', methods=['GET'])
+def home():
+    return 'Hello World!'
+
+
+
 @app.route('/webhook', methods=['POST'])
 def handle_webhook():
     data = request.json
+    print(data)
     # Do something with the webhook data
     return 'OK'
